3586e065b343231d9d40bba0d4814c7930a7b546
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-09 10:58:04 +0900

    cron-job.org 로 옮기며 cron 스케줄을 제거

--- moragi/__init__.py
+++ moragi/__init__.py
@@ -11,3 +11,11 @@ def get_meals_and_send_message(cj_fresh_meal_store_id: int, slack_webhook_url: s
     breafast_options = options['1']
     lunch_options = options['2']
     slack.send_slack_message(slack_webhook_url, breafast_options, lunch_options)
+
+
+@app.command()
+def get_photos_and_send_message(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+    options = cj_fresh_meal.get_food_options(cj_fresh_meal_store_id)
+    breafast_options = options['1']
+    lunch_options = options['2']
+    slack.send_slack_message(slack_webhook_url, breafast_options, lunch_options)
