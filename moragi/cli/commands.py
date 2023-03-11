f01a28e3ac77b611599cf203d17c3ac90ea1cec0
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-12 05:32:39 +0900

    Rename command name

--- moragi/cli/commands.py
+++ moragi/cli/commands.py
@@ -7,14 +7,17 @@ cli_app = Typer()
 
 
 @cli_app.command()
-def send_meal_message(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+def send_daily_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
     daily_menu = get_today_meal(cj_fresh_meal_store_id)
     sender = slack.MealSummarySender(slack_webhook_url, daily_menu)
     sender.run()
 
 
 @cli_app.command()
-def send_photo_message(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+def send_daily_meal_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhook_url: str):
     lunch_options = get_today_lunch_with_image(cj_fresh_meal_store_id)
     sender = slack.LunchWithPhotoSender(slack_webhook_url, lunch_options)
     sender.run()
+
+
+# todo: implement next meal (tommorow or next week monday)
