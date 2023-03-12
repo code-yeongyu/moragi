8c3e2a9883fbbb9da9d5ee5978c7b7e2a38b70a7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-12 17:50:36 +0900

    주석 추가

--- moragi/models/test/test_cj_fresh_meal_response_model.py
+++ moragi/models/test/test_cj_fresh_meal_response_model.py
@@ -4,6 +4,7 @@ from moragi.models.cj_fresh_meal_response_model import DayMeal
 
 
 def test_parse_day_menu_model():
+    """'1' 이나 '2' 같은 키값도 제대로 파싱하는지 테스트"""
     # given
     day_menu_response = json.dumps({
         '1': [{
