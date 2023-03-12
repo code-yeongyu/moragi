2c124944f2c532afe1d33842612089a02b56e2ed
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-12 21:02:21 +0900

    다음날 메뉴 보내는 command 추가

--- moragi/cli/commands.py
+++ moragi/cli/commands.py
@@ -1,7 +1,22 @@
+import datetime
+from typing import Optional
+
+import pytz
 from typer import Typer
 
 from moragi.cli.cj_fresh_meal_utils import get_today_lunch_with_image, get_today_meal
-from moragi.utils.slack import LunchWithPhotoMessageBuilder, MenuSummaryMessageBuilder, SlackMessageSender
+from moragi.models.cj_fresh_meal import WeekType
+from moragi.models.menu import DailyMenu
+from moragi.models.weekday import Weekday
+from moragi.utils.cj_fresh_meal import CJFreshMealClient
+from moragi.utils.slack import (
+    FridayAfternoonMessageBuilder,
+    LunchWithPhotoMessageBuilder,
+    MenuSummaryMessageBuilder,
+    SlackMessageBuilder,
+    SlackMessageSender,
+    TommorowMenuMessageBuilder,
+)
 
 cli_app = Typer()
 
@@ -24,4 +39,40 @@ def send_daily_meal_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhoo
     sender.run()
 
 
-# todo: implement next meal (tommorow or next week monday)
+@cli_app.command()
+def send_next_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+    # https://console.cron-job.org/jobs/4233822 (59 17 * * 1-5)
+    client = CJFreshMealClient(cj_fresh_meal_store_id)
+    builder: SlackMessageBuilder
+
+    is_today_friday = \
+        datetime.datetime.utcnow().astimezone(pytz.timezone('Asia/Seoul')).weekday() == Weekday.FRIDAY
+    if is_today_friday:
+        week_menu = client.get_week_meal(WeekType.NEXT_WEEK)
+        if not week_menu:
+            raise Exception("Error while getting next week's menu")
+        if not week_menu.monday:
+            raise Exception("Error while getting next week's monday's menu")
+        builder = FridayAfternoonMessageBuilder(week_menu.monday)
+    else:
+        week_menu = client.get_week_meal(WeekType.THIS_WEEK)
+        if not week_menu:
+            raise Exception('Error while getting next week menu')
+
+        weekday = Weekday(datetime.date.today().weekday())
+        tommorow_menu: Optional[DailyMenu] = None
+        if weekday == Weekday.MONDAY:
+            tommorow_menu = week_menu.tuesday
+        elif weekday == Weekday.TUESDAY:
+            tommorow_menu = week_menu.wednesday
+        elif weekday == Weekday.WEDNESDAY:
+            tommorow_menu = week_menu.thursday
+        elif weekday == Weekday.THURSDAY:
+            tommorow_menu = week_menu.friday
+
+        if not tommorow_menu:
+            raise Exception("Error while getting tommorow's menu")
+        builder = TommorowMenuMessageBuilder(tommorow_menu)
+
+    sender = SlackMessageSender(slack_webhook_url, builder)
+    sender.run()
