b3735f52b9e706fa93b111c66cdbbcec56455c93
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-12 21:02:03 +0900

    작업 cron 정보 주석으로 추가

--- moragi/cli/commands.py
+++ moragi/cli/commands.py
@@ -8,6 +8,7 @@ cli_app = Typer()
 
 @cli_app.command()
 def send_daily_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+    # https://console.cron-job.org/jobs/4229758 (59 7 * * 1-5)
     daily_menu = get_today_meal(cj_fresh_meal_store_id)
     builder = MealSummaryMessageBuilder(daily_menu)
     sender = SlackMessageSender(slack_webhook_url, builder)
@@ -16,6 +17,7 @@ def send_daily_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str)
 
 @cli_app.command()
 def send_daily_meal_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+    # https://console.cron-job.org/jobs/4229777 (29 11 * * 1-5)
     lunch_options = get_today_lunch_with_image(cj_fresh_meal_store_id)
     builder = LunchWithPhotoMessageBuilder(lunch_options)
     sender = SlackMessageSender(slack_webhook_url, builder)
