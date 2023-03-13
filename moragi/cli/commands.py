b5015530a12e1d9900bea00f595ba6f533ef665b
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-03-13 10:03:25 +0900

    사용하지 않는 command 삭제

--- moragi/cli/commands.py
+++ moragi/cli/commands.py
@@ -4,7 +4,7 @@ from typing import Optional
 import pytz
 from typer import Typer
 
-from moragi.cli.cj_fresh_meal_utils import get_today_lunch_with_image, get_today_meal
+from moragi.cli.cj_fresh_meal_utils import get_today_lunch_with_image
 from moragi.models import Weekday
 from moragi.models.cj_fresh_meal import WeekType
 from moragi.models.menu import DailyMenu
@@ -12,7 +12,6 @@ from moragi.utils.cj_fresh_meal import CJFreshMealClient
 from moragi.utils.slack import (
     FridayAfternoonMessageBuilder,
     LunchWithPhotoMessageBuilder,
-    MenuSummaryMessageBuilder,
     SlackMessageBuilder,
     SlackMessageSender,
     TommorowMenuMessageBuilder,
@@ -22,16 +21,7 @@ cli_app = Typer()
 
 
 @cli_app.command()
-def send_daily_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
-    # https://console.cron-job.org/jobs/4229758 (59 7 * * 1-5)
-    daily_menu = get_today_meal(cj_fresh_meal_store_id)
-    builder = MenuSummaryMessageBuilder(daily_menu)
-    sender = SlackMessageSender(slack_webhook_url, builder)
-    sender.run()
-
-
-@cli_app.command()
-def send_daily_meal_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+def send_daily_menu_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhook_url: str):
     # https://console.cron-job.org/jobs/4229777 (29 11 * * 1-5)
     lunch_options = get_today_lunch_with_image(cj_fresh_meal_store_id)
     builder = LunchWithPhotoMessageBuilder(lunch_options)
@@ -40,7 +30,7 @@ def send_daily_meal_summary_with_photo(cj_fresh_meal_store_id: int, slack_webhoo
 
 
 @cli_app.command()
-def send_next_meal_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
+def send_next_menu_summary(cj_fresh_meal_store_id: int, slack_webhook_url: str):
     # https://console.cron-job.org/jobs/4233822 (59 17 * * 1-5)
     client = CJFreshMealClient(cj_fresh_meal_store_id)
     builder: SlackMessageBuilder
