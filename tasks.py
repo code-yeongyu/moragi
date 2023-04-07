fea9ac39216e900b3fc0dab737f13b9ed03c4061
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-04-07 11:12:22 +0900

    invoke 에서 필요없는 명령어 삭제

--- tasks.py
+++ tasks.py
@@ -1,9 +1,7 @@
 # type: ignore
-import shutil
 
 import toml
 from invoke import Context, task
-from invoke.exceptions import UnexpectedExit
 
 import monkey_patch_invoke as _  # noqa: F401
 
@@ -17,11 +15,6 @@ def get_project_path():
         data = toml.load(file)
         project_name = get_pep8_compliant_name(data['tool']['poetry']['name'])
         return project_name
-
-
-@task
-def run(context: Context):
-    context.run(f'python -m {get_project_path()}', pty=True)
 
 
 @task
@@ -61,55 +54,3 @@ def check_code_style(context: Context):
 @task
 def check_types(context: Context):
     context.run(f'pyright {get_project_path()}', pty=True)
-
-
-@task
-def rename_project(_context: Context, project_name: str):
-    # Get the current project path
-    current_project_path = get_project_path()
-
-    # Rename the directory
-    shutil.move(get_pep8_compliant_name(current_project_path), get_pep8_compliant_name(project_name))
-
-    # Update the project name in pyproject.toml
-    with open('pyproject.toml', 'r', encoding='utf-8') as file:
-        data = toml.load(file)
-
-    data['tool']['poetry']['name'] = project_name
-
-    with open('pyproject.toml', 'w', encoding='utf-8') as file:
-        toml.dump(data, file)
-
-
-@task
-def release(context: Context, version: str) -> None:
-    '''Build & Publish to PyPI.'''
-
-    # load pyproject
-    pyproject_path = 'pyproject.toml'
-    pyproject_string = ''
-    with open(pyproject_path, 'r', encoding='utf-8') as pyproject_file:
-        pyproject_string = pyproject_file.read()
-    pyproject = toml.loads(pyproject_string)
-    # change version to today datetime
-    pyproject['tool']['poetry']['version'] = version
-    with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
-        toml.dump(pyproject, pyproject_file)
-
-    # build & publish
-    try:
-        context.run(
-            '''
-                poetry build --no-interaction
-                poetry publish --no-interaction
-            ''',
-            pty=True,
-        )
-    except UnexpectedExit as exception:
-        with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
-            pyproject_file.write(pyproject_string)
-        raise exception from exception
-
-    # recover to original
-    with open(pyproject_path, 'w', encoding='utf-8') as pyproject_file:
-        pyproject_file.write(pyproject_string)
