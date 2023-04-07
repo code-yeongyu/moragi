5684b71c6fd1a7e8c16a141f5d681f1f6b3c9a8f
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2023-04-07 11:14:38 +0900

    사용하지 않는 block 삭제

--- moragi/utils/slack/blocks.py
+++ moragi/utils/slack/blocks.py
@@ -5,21 +5,6 @@ from moragi.models.menu import DailyMenu, Menu
 SLACK_BLOCK_TYPE = list[dict[str, Any]]
 
 
-def simple_menu_block(menu: Menu):
-    return {
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': f'''
-*{menu.food_type}*
-• {menu.name}
-• {menu.side}
-_{menu.kcal} 칼로리_
-'''[1:]
-        }
-    }
-
-
 def image_menu_block(menu: Menu):
     return [{
         'type': 'section',
@@ -46,21 +31,6 @@ def image_menu_block(menu: Menu):
             'url': menu.detail_info_url
         }]
     }]
-
-
-def simple_menu_list_block(menu_list: list[Menu]):
-    return [{
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': f'''
-*{menu.food_type}*
-• {menu.name}
-• {menu.side}
-_{menu.kcal} 칼로리_
-'''[1:]
-        }
-    } for menu in menu_list]
 
 
 def section_menu_list_block(menu_list: list[Menu]):
